ef9c70801f98bdcc9949442394d3787a23715b60
dionysus <dionysus.cho@gmail.com>
2017-01-10 00:06:34 -0800

    Colliders added

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	//public static int players = 0;
+
+	static MusicPlayer instance = null;
+
+	void Awake (){
+	
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Destroyed MPlayer");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
